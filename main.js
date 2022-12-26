c1a2bb7888f26cbb2420e6d39655b36dd9eec328
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2022-12-26 16:49:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //https://teachablemachine.withgoogle.com/models/xsXbeonwW/
+
+function startClassifictaion() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/xsXbeonwW/model.json',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
